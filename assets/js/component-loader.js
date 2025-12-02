9637c09d4eac6d214bcd54bf3b5ddb93c9a93281
Vovasko <vovasko16@gmail.com>
2025-12-02 21:30:22 +0100

    Relative links in header fix

--- assets/js/component-loader.js
+++ assets/js/component-loader.js
@@ -1,11 +1,11 @@
 function loadComponent(id, file) {
-  const fileName = window.location.pathname.split("/").pop() || "index.html";
+  // const fileName = window.location.pathname.split("/").pop() || "index.html";
 
-  const filePath = fileName === "index.html"
-    ? file               // if index.html, use file as-is
-    : "../" + file;      // if not, prefix with "../"
+  // const filePath = fileName === "index.html"
+  //   ? file               // if index.html, use file as-is
+  //   : "../" + file;      // if not, prefix with "../"
 
-  fetch(filePath)
+  fetch(file)
     .then(response => response.text())
     .then(data => {
       document.getElementById(id).innerHTML = data;
@@ -18,17 +18,17 @@ function loadComponent(id, file) {
 loadComponent("header-placeholder", "components/header.html");
 loadComponent("footer-placeholder", "components/footer.html");
 
-document.addEventListener("headerLoaded", () => {
-  console.log("headerLoaded event detected");
-  const isHome = window.location.pathname === "/" || window.location.pathname.endsWith("index.html");
+// document.addEventListener("headerLoaded", () => {
+//   console.log("headerLoaded event detected");
+//   const isHome = window.location.pathname === "/" || window.location.pathname.endsWith("index.html");
 
-  document.querySelectorAll(".homeLink").forEach(link => {
-    const target = link.getAttribute("href"); // e.g. "#about"
-    link.setAttribute("href", isHome ? target : "../index.html" + target);
-  });
+//   document.querySelectorAll(".homeLink").forEach(link => {
+//     const target = link.getAttribute("href"); // e.g. "#about"
+//     link.setAttribute("href", isHome ? target : "../index.html" + target);
+//   });
 
-  document.querySelectorAll(".assetLink").forEach(link => {
-    const target = link.getAttribute("href"); 
-    link.setAttribute("href", isHome ? target : "../" + target);
-  });
-});
+//   document.querySelectorAll(".assetLink").forEach(link => {
+//     const target = link.getAttribute("href"); 
+//     link.setAttribute("href", isHome ? target : "../" + target);
+//   });
+// });
